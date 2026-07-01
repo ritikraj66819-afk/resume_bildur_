77e595ad51f1e633d1cac22384a6bdd2bb86ab6e
ritikraj66819-afk <supriyadevi66819@gmail.com>
2026-07-01 13:54:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-body {
-  font-family: Arial;
-}
+<!DOCTYPE html>
+<html>
+<head>
+    <title>AI Resume Builder</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h1>AI Resume Builder</h1>
+
+<p>Enter Your Name:</p>
+<input type="text" id="name" placeholder="Your Name">
+
+<br><br>
+
+<button onclick="createResume()">Create Resume</button>
+
+<h2 id="result"></h2>
+
+<script src="script.js"></script>
+
+</body>
+</html>
